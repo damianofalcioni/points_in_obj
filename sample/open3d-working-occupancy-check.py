8a5e52eadefbe0d2618170cb79444489fdc43114
Damiano Falcioni <damianofalcioni@users.noreply.github.com>
2023-09-21 12:43:34 +0200

    updated description

--- sample/open3d-working-occupancy-check.py
+++ sample/open3d-working-occupancy-check.py
@@ -1,10 +1,11 @@
+#Dependencies:
+# pip install open3d
+#Requirement: 
+# OBJ must contain mesh of triangules
 import open3d as o3d
-
 mesh = o3d.io.read_triangle_mesh("Sendlinger_Tor_Munich_ZoneOnly_brg.obj")
 mesh = o3d.t.geometry.TriangleMesh.from_legacy(mesh)
 scene = o3d.t.geometry.RaycastingScene()
 scene.add_triangles(mesh)
-
-
 occupancy = scene.compute_occupancy(o3d.core.Tensor([[16.81775, 33.73125, 512.21]], dtype=o3d.core.Dtype.Float32))
 print("occupancy", occupancy.numpy())
